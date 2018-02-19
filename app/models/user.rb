bae5d47439cef05df189d351675df63bac22d03a
Owen W. Krueger <okrueger14@gmail.com>
2018-02-19 17:39:56 -0500

    added comments and associated relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  # Rails representes its relations through these statements
+  # Available relations are 
+  # belongs_to - (is dependent upon object)
+  # has_one - (Is owner of a single object)
+  # has_many - (Is owner of multiple objects)
+  # has_many :through - (Is owner of multiple objects through an associative entity)
+  # has_one :through - (Is owner of one object through an associative entity)
+  # has_and_belongs_to_many - (Database Spaghetti)
+  # if you didnt understand all of this please read http://guides.rubyonrails.org/association_basics.html#the-types-of-associations
+  
+  #the user object owns many posts
   has_many :posts
+
+  #the user object owns many comments
+  # If a user is deleted, this will cause a deletion of their comments as well.
+  has_many :comments, dependent: :destory
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180218175004) do
+ActiveRecord::Schema.define(version: 20180219215925) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
